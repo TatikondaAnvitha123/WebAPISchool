b836cc444ced4e631cf76d4f7dec534dd2be80c6
TatikondaAnvitha123 <tatikondaanvitha05@gmail.com>
2024-02-12 14:36:04 +0530

    Add project files.

---
+++ Data/SchoolDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPISchool.Models;
+
+namespace WebAPISchool.Data
+{
+    public class SchoolDbContext : DbContext
+    {
+        public SchoolDbContext (DbContextOptions<SchoolDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAPISchool.Models.School> School { get; set; } = default!;
+    }
+}
---
+++ Models/School.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPISchool.Models
+{
+    [Table("School")]
+    public class School
+    {
+        [Key]
+        public int RollNo { get; set; }
+        public int SchoolId { get; set; }
+        public int marks { get; set; }
+        public string Student { get; set; }
+        public string Subject { get; set; }
+    }
+}
